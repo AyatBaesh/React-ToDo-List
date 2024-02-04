16a431f2bb4d68d553f12ef0159bc63d38fc3bab
romanyaremchuk <romanza33@gmail.com>
2024-02-04 15:42:47 +0200

    Commenting error

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,11 +1,16 @@
+//TODO causes error "each child in a list should have uniqe key"
+// probably it is caused by tasks.map
+
 import Task from "./Task.jsx";
 export default function Category({ name, tasks }) {
   return (
-    <div className={name}>
-      <h2>{name}</h2>
-      {tasks.map((task) => {
-        return <Task task={task} />;
-      })}
-    </div>
+    <>
+      <div className={name}>
+        <h2>{name}</h2>
+        {tasks.map((task) => {
+          return <Task task={task} />;
+        })}
+      </div>
+    </>
   );
 }
